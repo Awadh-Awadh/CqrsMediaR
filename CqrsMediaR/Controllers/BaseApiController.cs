579084646742d5ec40bfad1e4e57c842173b4592
Awadh Awadh <awadhawadh84@yahoo.com>
2022-08-24 22:54:12 +0300

    add a base Api controller

---
+++ CqrsMediaR/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsMediaR.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class BaseApiController : Controller
+{
+    private ISender? _mediator;
+
+    protected ISender Mediator => _mediator;
+
+}
