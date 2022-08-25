a8195e5aa3ed9ca4d5524613345e68d020b444fa
Awadh Awadh <awadhawadh84@yahoo.com>
2022-08-25 22:50:30 +0300

    create a handler for fetching the person

---
+++ Shared/Handers/GetPersonHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Shared.Exceptions;
+using Shared.Models;
+using Shared.Queries;
+
+namespace Shared.Handers;
+
+
+
+public record GetPersonHandler : IRequestHandler<GetPersonQuery, Person>
+{
+    private readonly IMediator _mediator;
+
+    public GetPersonHandler(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    public async Task<Person> Handle(GetPersonQuery request, CancellationToken cancellationToken)
+    {
+        var persons = await _mediator.Send(new GetAllPersonsQuery());
+        
+        var person =  persons.FirstOrDefault(c => c.Id == request.Id);
+
+        return person ?? throw new PersonNotFoundException("Person not found");
+
+
+
+    }
+}
