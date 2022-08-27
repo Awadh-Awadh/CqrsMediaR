3fc61ff81848f1ecbb6c192339f7a874ce7cca6f
Awadh Awadh <awadhawadh84@yahoo.com>
2022-08-27 20:39:36 +0300

    add create person handler

---
+++ Shared/Handers/CreatePersonHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Shared.Commands;
+using Shared.DTOs;
+using Shared.Infra;
+using Shared.Models;
+
+namespace Shared.Handers;
+
+public class CreatePersonHandler : IRequestHandler<CreatePersonCommand, Person>
+
+{
+    private readonly IDataAccess _dataAccess;
+
+    public CreatePersonHandler(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+    public async Task<Person> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
+    {
+        var person = new Person(0, request.FirstName, request.LastName);
+
+        _dataAccess.InsertPerson(person);
+
+        return person;
+
+    }
+}
+
