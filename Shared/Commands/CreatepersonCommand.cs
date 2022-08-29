7865023f4f9bd68e4888014ffadaa6dd8b466975
Awadh Awadh <awadhawadh84@yahoo.com>
2022-08-29 22:02:06 +0300

    add create person commands

--- Shared/Commands/CreatepersonCommand.cs
+++ Shared/Commands/CreatepersonCommand.cs
@@ -4,4 +4,6 @@ using Shared.Models;
 
 namespace Shared.Commands;
 
+// need to pass a dto instead of the entire entity 
+// and return a dto
 public record CreatePersonCommand (string FirstName, string LastName): IRequest<Person>;
