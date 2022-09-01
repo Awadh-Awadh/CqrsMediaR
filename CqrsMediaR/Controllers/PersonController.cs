6c5214aa32963d30de30527d283463c24fa52590
Awadh Awadh <awadhawadh84@yahoo.com>
2022-09-01 14:57:19 +0300

    pulish notifications

--- CqrsMediaR/Controllers/PersonController.cs
+++ CqrsMediaR/Controllers/PersonController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Shared.Commands;
 using Shared.DTOs;
 using Shared.Models;
+using Shared.Notifications;
 using Shared.Queries;
 
 namespace CqrsMediaR.Controllers;
@@ -13,8 +14,13 @@ public class PersonController : Controller
 
 {
     private readonly IMediator _mediator;
+    private readonly IPublisher _publisher;
 
-    public PersonController( IMediator mediator) => _mediator = mediator;
+    public PersonController(IMediator mediator, IPublisher publisher)
+    {
+        _mediator = mediator;
+        _publisher = publisher;
+    } 
    
     [HttpGet]
 
@@ -48,6 +54,8 @@ public class PersonController : Controller
     public async Task<ActionResult<CreatePersonDTO>> CreatePerson([FromBody]CreatePersonDTO request)
     {
         var model = new CreatePersonCommand( request.FirstName, request.LastName);
+
+        await  _publisher.Publish(new PersonCreatedNotification(model.FirstName, model.LastName));
         
         return Ok(await _mediator.Send(model));
 
