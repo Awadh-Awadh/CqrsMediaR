ebd0ee4ddebedbc96d9e6ee8d2aa972c67f7610a
Awadh Awadh <awadhawadh84@yahoo.com>
2022-09-01 14:56:04 +0300

    invalidate cache after a person is created

---
+++ Shared/NotificationHandlers/CacheInvalidationHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Shared.Infra;
+using Shared.Models;
+using Shared.Notifications;
+
+namespace Shared.NotificationHandlers;
+
+public class CacheInvalidationHandler : INotificationHandler<PersonCreatedNotification>
+{
+    private readonly IDataAccess _dataAccess;
+
+    public CacheInvalidationHandler(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+    public async Task Handle(PersonCreatedNotification notification, CancellationToken cancellationToken)
+    {
+        Person person = new()
+        {
+            FirstName = notification.FirstName,
+            LastName = notification.LastName
+        };
+
+        await _dataAccess.EventOccured(person, "Cache Invalidated");
+        await Task.CompletedTask;
+
+    }
+}
