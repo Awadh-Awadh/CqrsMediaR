e34d012aabca1b2c979a4834a2a2c37f59dd3318
Awadh Awadh <awadhawadh84@yahoo.com>
2022-09-01 14:55:30 +0300

    sent email immediately after a person is created

---
+++ Shared/NotificationHandlers/PersonCreatedHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Shared.Infra;
+using Shared.Models;
+using Shared.Notifications;
+
+namespace Shared.NotificationHandlers;
+
+public class EmailHandler: INotificationHandler<PersonCreatedNotification>
+
+{
+    private readonly IDataAccess _dataAccess;
+
+    public EmailHandler(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+    public async Task Handle(PersonCreatedNotification notification, CancellationToken cancellationToken)
+    {
+        Person person = new()
+        {
+            FirstName = notification.FirstName,
+            LastName = notification.LastName
+        };
+        
+        // event occured can be a service for example send email service
+        
+       await  _dataAccess.EventOccured(person, "Email Sent");
+    }
+}
