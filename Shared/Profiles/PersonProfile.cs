ebb38433647a238899e060c8c45058cce53698df
Awadh Awadh <awadhawadh84@yahoo.com>
2022-09-06 19:51:38 +0300

    add automapper and person profiles

---
+++ Shared/Profiles/PersonProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace Shared.Profiles;
+
+public class PersonProfile : Profile
+{
+    public PersonProfile()
+    { 
+        // souce -> destination 
+        
+        CreateMap<Person, PersonReadDto>();
+        
+       // CreateMap<CreatePersonDto, Person>();
+    }
+}
